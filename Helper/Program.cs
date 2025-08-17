684131da90d51c4882f8cf40092e506dc6778569
dmitrybahtenkov <d.bahtenkov@skillaz.ru>
2025-08-17 22:15:14 +0300

    фиксы

---
+++ Helper/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+
+var file = "/Users/dmitrybahtenkov/RiderProjects/Letovo/OmniscanAdmin/chips.json";
+
+var chips = JsonSerializer.Deserialize<List<Chip>>(File.ReadAllText(file));
+
+var random = new Random();
+var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+if (chips != null)
+{
+    foreach (var chip in chips)
+    {
+        chip.DisableCode = new string(Enumerable.Repeat(chars, 8)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+
+    var options = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
+
+    var updatedJson = JsonSerializer.Serialize(chips, options);
+    File.WriteAllText(file, updatedJson);
+
+    Console.WriteLine($"Успешно обновлено {chips.Count} чипов с новыми кодами отключения.");
+
+    foreach (var chip in chips)
+    {
+        Console.WriteLine($"ID: {chip.Id}, Name: {chip.Name}, DisableCode: {chip.DisableCode}");
+    }
+}
+
+public class Chip
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Status { get; set; } = "";
+    public int StatusCode { get; set; }
+    public string LastCommand { get; set; } = "";
+    public DateTime LastUpdate { get; set; }
+    public string SerialNumber { get; set; } = "";
+    public string DisableCode { get; set; } = "";
+}
---
+++ OmniscanAdmin/Models/SystemConfig.cs
@@ -0,0 +1,26 @@
+namespace OmniscanAdmin.Models
+{
+    public static class SystemConfig
+    {
+        private static bool _allowDisable = true;
+        private static readonly object _lock = new object();
+
+        public static bool AllowDisable
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _allowDisable;
+                }
+            }
+            set
+            {
+                lock (_lock)
+                {
+                    _allowDisable = value;
+                }
+            }
+        }
+    }
+}
